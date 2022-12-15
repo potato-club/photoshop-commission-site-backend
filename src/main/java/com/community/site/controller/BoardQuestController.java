a938d89be3f1c036cfd674c4e1d81c5a0203cac6
JOE1534 <king01286@naver.com>
2022-12-16 02:26:14 +0900

    #8 Bug Fix - 만료된 토큰으로 의뢰 신청 API 호출 시 토큰 자동 재발급 후 진행

--- src/main/java/com/community/site/controller/BoardQuestController.java
+++ src/main/java/com/community/site/controller/BoardQuestController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -21,26 +22,29 @@ public class BoardQuestController {
     private final BoardQuestService boardQuestService;
 
     @GetMapping("/list/{id}/request")
-    public Page<UserNicknameDto> getRequestUserList(@PathVariable Long id, @RequestParam("page") int page, HttpServletRequest request) {
-        return boardQuestService.getRequestUserList(id, page, request);
+    public Page<UserNicknameDto> getRequestUserList(@PathVariable Long id, @RequestParam("page") int page,
+                                                    HttpServletRequest request, HttpServletResponse response) {
+        return boardQuestService.getRequestUserList(id, page, request, response);
     }
 
     @PostMapping("/list/{id}/request")
-    public ResponseEntity<String> acceptQuest(@PathVariable Long id, HttpServletRequest request) {
-        boardQuestService.acceptQuest(id, request);
+    public ResponseEntity<String> acceptQuest(@PathVariable Long id, HttpServletRequest request,
+                                              HttpServletResponse response) {
+        boardQuestService.acceptQuest(id, request, response);
         return ResponseEntity.ok("리스트에 등록됨");
     }
 
     @PutMapping("/list/{id}/request")
-    public ResponseEntity<String> exceptQuest(@PathVariable Long id, HttpServletRequest request) {
-        boardQuestService.exceptQuest(id, request);
+    public ResponseEntity<String> exceptQuest(@PathVariable Long id, HttpServletRequest request,
+                                              HttpServletResponse response) {
+        boardQuestService.exceptQuest(id, request, response);
         return ResponseEntity.ok("리스트에서 제거됨");
     }
 
     @PostMapping("/list/{id}/artist")
     public ResponseEntity<String> chooseArtist(@PathVariable Long id, @RequestBody UserNicknameDto artistDto,
-                                               HttpServletRequest request) {
-        boardQuestService.chooseArtist(id, artistDto, request);
+                                               HttpServletRequest request, HttpServletResponse response) {
+        boardQuestService.chooseArtist(id, artistDto, request, response);
         return ResponseEntity.ok("ARTIST 선택 완료");
     }
 }
--- src/main/java/com/community/site/service/BoardQuestService.java
+++ src/main/java/com/community/site/service/BoardQuestService.java
@@ -6,6 +6,7 @@ import com.community.site.dto.UserDto.UserNicknameDto;
 import com.community.site.entity.BoardList;
 import com.community.site.entity.User;
 import com.community.site.enumcustom.BoardEnumCustom;
+import com.community.site.error.exception.DuplicateException;
 import com.community.site.error.exception.UnAuthorizedException;
 import com.community.site.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,6 +27,7 @@ import java.util.stream.Collectors;
 
 import static com.community.site.enumcustom.UserRole.ARTIST;
 import static com.community.site.error.ErrorCode.ACCESS_DENIED_EXCEPTION;
+import static com.community.site.error.ErrorCode.CONFLICT_EXCEPTION;
 
 
 @Slf4j
@@ -35,6 +38,7 @@ public class BoardQuestService {
 
     private final BoardRepository boardRepository;
     private final UserRepository userRepository;
+    private final TokenService tokenService;
     private final JwtTokenProvider jwtTokenProvider;
 
     private BoardList getBoardList(Long id) {
@@ -44,7 +48,11 @@ public class BoardQuestService {
         });
     }
 
-    private User getUserByEmail(String email) {
+    @Transactional
+    public User getUserByToken(HttpServletRequest request,
+                                HttpServletResponse response) {
+        String token = tokenService.validateAndReissueToken(request, response);
+        String email = jwtTokenProvider.getUserEmail(token);
         return userRepository.findByEmail(email).orElseThrow(() ->
         {
             throw new UnAuthorizedException("E0002", ACCESS_DENIED_EXCEPTION);
@@ -61,11 +69,11 @@ public class BoardQuestService {
     }
 
     @Transactional
-    public Page<UserNicknameDto> getRequestUserList(Long id, int page, HttpServletRequest request) {
-        String email = jwtTokenProvider.getUserEmail(jwtTokenProvider.resolveAccessToken(request));
+    public Page<UserNicknameDto> getRequestUserList(Long id, int page, HttpServletRequest request,
+                                                    HttpServletResponse response) {
         BoardList boardList = getBoardList(id);
         List<UserNicknameDto> requestList;
-        User user = getUserByEmail(email);
+        User user = getUserByToken(request, response);
 
         if (!boardList.getUser().equals(user)) {
             throw new UnAuthorizedException("게시글 작성자만 가능합니다.", ACCESS_DENIED_EXCEPTION);
@@ -78,25 +86,25 @@ public class BoardQuestService {
     }
 
     @Transactional
-    public void acceptQuest(Long id, HttpServletRequest request) {
-        String email = jwtTokenProvider.getUserEmail(jwtTokenProvider.resolveAccessToken(request));
+    public void acceptQuest(Long id, HttpServletRequest request,
+                            HttpServletResponse response) {
         BoardList boardList = getBoardList(id);
-        User user = getUserByEmail(email);
+        User user = getUserByToken(request, response);
 
         if (!user.getUserRole().equals(ARTIST)) {
             throw new UnAuthorizedException("ARTIST 유저만 가능합니다", ACCESS_DENIED_EXCEPTION);
         } else if(boardList.getRequestList().contains(user.getNickname())) {
-            throw new UnAuthorizedException("이미 요청된 상태입니다.", ACCESS_DENIED_EXCEPTION);
+            throw new DuplicateException("이미 요청된 상태입니다.", CONFLICT_EXCEPTION);
         }
 
         boardList.addAcceptQuest(user.getNickname());
     }
 
     @Transactional
-    public void exceptQuest(Long id, HttpServletRequest request) {
-        String email = jwtTokenProvider.getUserEmail(jwtTokenProvider.resolveAccessToken(request));
+    public void exceptQuest(Long id, HttpServletRequest request,
+                            HttpServletResponse response) {
         BoardList boardList = getBoardList(id);
-        User user = getUserByEmail(email);
+        User user = getUserByToken(request, response);
 
         if (!user.getUserRole().equals(ARTIST)) {
             throw new UnAuthorizedException("ARTIST 유저만 가능합니다", ACCESS_DENIED_EXCEPTION);
@@ -108,11 +116,11 @@ public class BoardQuestService {
     }
 
     @Transactional
-    public void chooseArtist(Long id, UserNicknameDto artistDto, HttpServletRequest request) {
+    public void chooseArtist(Long id, UserNicknameDto artistDto, HttpServletRequest request,
+                             HttpServletResponse response) {
         User artist = userRepository.findByNickname(artistDto.getNickname());
-        String email = jwtTokenProvider.getUserEmail(jwtTokenProvider.resolveAccessToken(request));
         BoardList boardList = getBoardList(id);
-        User user = getUserByEmail(email);
+        User user = getUserByToken(request, response);
 
         if (!boardList.getUser().equals(user)) {
             throw new UnAuthorizedException("게시글 작성자만 가능합니다.", ACCESS_DENIED_EXCEPTION);
